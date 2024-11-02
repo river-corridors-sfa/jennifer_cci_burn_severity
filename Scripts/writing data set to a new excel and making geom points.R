6de30c91346846a9ebd8894ec8cfa3d480e34775
jmoreno7615 <jennifer.moreno7615@gmail.com>
2024-11-01 21:14:10 -0700

    Updated dam script
    added code for creat geom sf points for plotting dams

---
+++ Scripts/writing data set to a new excel and making geom points.R
@@ -0,0 +1,44 @@
+
+#Title: Making a Dam Map and geom points
+#Author: Jennifer Moreno-Ramirez (CCI)
+#Description:
+# Script for snatching specific columns from the NID database and geom points from long and lat
+
+# packages:
+# file reader
+install.packages("data.table")
+# creates new excel sheet
+install.packages("openxlsx")
+library("data.table")
+library("openxlsx")
+library("readxl")
+library("sf")
+library("tidyverse")
+library("nhdplusTools")
+
+# pulling up the dam file and desired columns
+dams <- fread("dams.csv", select = c("Dam Name", "Latitude", "Longitude", "River or Stream Name", "City", "State"))
+# used the dam object (haha) and wrote the columns onto a new excel
+write.xlsx(dams, 'Damlocationsandstreamnames.xlsx')
+
+local_path <- "data/mtbs/wa4685412079920200831/"
+
+# Set common coordinate reference system
+# for any spacial things
+common_crs = 4326
+
+# make sure you are reading in the file, which makes a data frame
+# make sure excel is not open in any tabs, will block the read
+damsdb <- read_excel(path = "C:/Users/more173/OneDrive - PNNL/Documents/GitHub/rc_sfa-rc-3-wenas-modeling/data/Damlocationsandstreamnames.xlsx")
+
+# creates points for every dam that can be plotted 
+# na.omit omits dams that don't have lat/long values
+dams_sf <- st_as_sf(na.omit(damsdb), coords = c("Latitude", "Longitude"), crs = common_crs)
+
+#writes the sf into a .csv file, with coordinates separated into latidude and longitude columns
+st_write(dams_sf, "Damcoords.csv", layer_options = "GEOMETRY=AS_XY")
+
+#writes the sf into a .csv file with coordinates in a geom point column
+# from https://github.com/r-spatial/sf/issues/284
+st_write(dams_sf, "Damcoordinates.csv", layer_options = "GEOMETRY=AS_WKT")
+x <- st_read("Damcoordinates.csv", options = "GEOM_POSSIBLE_NAMES=WKT")
