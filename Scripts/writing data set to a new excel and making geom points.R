bf0f3c94edd7b569adac41b369247b000877aa44
jmoreno7615 <jennifer.moreno7615@gmail.com>
2024-11-25 11:58:06 -0800

    added more columns with info to the damm coord file

--- Scripts/writing data set to a new excel and making geom points.R
+++ Scripts/writing data set to a new excel and making geom points.R
@@ -17,7 +17,7 @@ library("tidyverse")
 library("nhdplusTools")
 
 # pulling up the dam file and desired columns
-dams <- fread("dams.csv", select = c("Dam Name", "Latitude", "Longitude", "River or Stream Name", "City", "State"))
+dams <- fread("dams.csv", select = c("Dam Name", "Latitude", "Longitude", "River or Stream Name", "City", "State", "NID Height (Ft)", "NID Height Category", "Dam Length (Ft)", "NID Storage (Acre-Ft)"))
 # used the dam object (haha) and wrote the columns onto a new excel
 write.xlsx(dams, 'Damlocationsandstreamnames.xlsx')
 
@@ -29,16 +29,16 @@ common_crs = 4326
 
 # make sure you are reading in the file, which makes a data frame
 # make sure excel is not open in any tabs, will block the read
-damsdb <- read_excel(path = "C:/Users/more173/OneDrive - PNNL/Documents/GitHub/rc_sfa-rc-3-wenas-modeling/data/Damlocationsandstreamnames.xlsx")
+damsdb <- read_excel(path = "C:/Users/more173/OneDrive - PNNL/Documents/Damlocationsandstreamnamesupdated.xlsx")
 
 # creates points for every dam that can be plotted 
 # na.omit omits dams that don't have lat/long values
 dams_sf <- st_as_sf(na.omit(damsdb), coords = c("Latitude", "Longitude"), crs = common_crs)
 
 #writes the sf into a .csv file, with coordinates separated into latidude and longitude columns
-st_write(dams_sf, "Damcoords.csv", layer_options = "GEOMETRY=AS_XY")
+st_write(dams_sf, "Damcoordsupdated.csv", layer_options = "GEOMETRY=AS_XY")
 
 #writes the sf into a .csv file with coordinates in a geom point column
 # from https://github.com/r-spatial/sf/issues/284
-st_write(dams_sf, "Damcoordinates.csv", layer_options = "GEOMETRY=AS_WKT")
+st_write(dams_sf, "Damcoordinatesupdated.csv", layer_options = "GEOMETRY=AS_WKT")
 x <- st_read("Damcoordinates.csv", options = "GEOM_POSSIBLE_NAMES=WKT")
